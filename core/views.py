eb41ad629689511e4a7d1469cdc9fae93ee4b94b
AY0OLA <ayoolaokunow@gmail.com>
2026-03-24 00:33:00 +0100

    adding items to db

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from item.models import Category, Items
+def index(request): 
+    items = Items.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html',{
+        'category': categories,
+        'items': items
+    })
+
+def contact(request):
+
+    return render(request, 'core/contact.html')
