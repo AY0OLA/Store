97f0f04d45179aa4eab34cf81a863ac5c2298ac4
AY0OLA <ayoolaokunow@gmail.com>
2026-03-26 18:08:42 +0100

    add logout button

--- core/views.py
+++ core/views.py
@@ -1,32 +1,31 @@
 from django.shortcuts import render, redirect
 from item.models import Category, Items
 from .forms import SignupForm
-
+from django.contrib.auth import logout
 
 def index(request): 
     items = Items.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
-    return render(request, 'core/index.html',{
+    return render(request, 'core/index.html', {
         'category': categories,
         'items': items
     })
 
 def contact(request):
-
     return render(request, 'core/contact.html')
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
-
         if form.is_valid():
             form.save()
-
-            return redirect('/login/')
+            return redirect('core:login')
     else:
         form = SignupForm()
-
-
-    return render(request, 'core/signup.html',{
+    return render(request, 'core/signup.html', {
         'form': form
     })
+
+def logout_view(request):
+    logout(request)
+    return redirect('core:login')  # redirect to the login page
